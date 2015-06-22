8128743a90c4c8b31707ceeddc9fcc467a47acc5
Pavel Podkorytov <pod.pavel@gmail.com>
2015-06-22 17:12:03 +0500

    nmap-scan-net: rewrite with nmap -oG

--- bin/bin/nmap-scan-net.py
+++ bin/bin/nmap-scan-net.py
@@ -4,55 +4,65 @@ from datetime import datetime
 from os.path import isfile
 from sys import exit
 import argparse
-import subprocess, re
+import subprocess
+import json
 
 NMAP='/usr/bin/nmap'
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Network scanner', formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-t', '--time', help='Add time to the output', action='store_true')
-    parser.add_argument('-f', '--fabric', help='Output text in fabric-like hosts format (host1,host2)', action='store_true')
-    parser.add_argument('-n', '--hostname', help='Show ip and hostname (do no use with -f option)', action='store_true')
-    parser.add_argument('-d', '--debug', help='Print raw text for debug', action='store_true')
-    parser.add_argument('subnet', type=str, help='Network subnet (for example 10.1.1.0/24)')
+    parser = argparse.ArgumentParser(description='Network scanner',
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-t', '--time', action='store_true',
+        help='Add time to the output')
+    parser.add_argument('-o', '--output', default='default',
+        choices=['fabric', 'json', 'default'],
+        help='Output format: fabric, json, default')
+    parser.add_argument('-d', '--debug', action='store_true',
+        help='Print raw text for debug')
+    parser.add_argument('subnet', type=str,
+        help='Network subnet (for example 10.1.1.0/24)')
     parser.set_defaults(func=print_output)
     return parser.parse_args()
 
 def print_output(args):
     output = get_output(args)
 
-    # add time to the output
     if args.time:
-        print('Date: {0}'.format(datetime.now()))
+        print output.split('\n')[0]
 
-    if args.fabric:
-        # fabric-like output format: host1,host2,host3
+    if args.output == 'json':
+        result = {}
+        for line in output.split('\n'):
+            if line.startswith('Host'):
+                _line = line.split()
+                result[_line[1]] = {}
+                result[_line[1]]['hostname'] = _line[2].replace('(','').replace(')','')
+                result[_line[1]]['status'] = _line[4]
+        print json.dumps(result, indent=4)
+    elif args.output == 'fabric':
         result = []
-        for line in output:
-            result.append(line[0])
+        for line in output.split('\n'):
+            if line.startswith('Host'):
+                _line = line.split()
+                result.append(_line[1])
         print(','.join(result))
     else:
-        # simple output format
-        for line in output:
-            print('{0: <16} {1}'.format(line[0], line[1]))
+        for line in output.split('\n'):
+            if line.startswith('Host'):
+                print line
 
 def get_output(args):
     try:
-        if args.hostname:
-            cmd = subprocess.check_output('{1} -sP {0}'.format(args.subnet, NMAP), stderr=subprocess.STDOUT, shell=True)
-        else:
-            cmd = subprocess.check_output('{1} -sP -n {0}'.format(args.subnet, NMAP), stderr=subprocess.STDOUT, shell=True)
-        output = re.findall('Nmap scan report for (.*)\nHost is ([up|down].*)', cmd, re.MULTILINE)
-        output.sort()
-        if args.debug:
-            print('DEBUG: raw subprocess output')
-            print(cmd)
-            print('DEBUG: raw output after regex and sorting')
-            print(output)
-        return output
+        output = subprocess.check_output('{1} -sP {0} -oG -'.format(
+            args.subnet, NMAP), stderr=subprocess.STDOUT, shell=True)
     except:
         print('Error in running {0}'.format(NMAP))
         exit(3)
+
+    if args.debug:
+        print('DEBUG: raw output after regex and sorting')
+        print(output)
+    return output
 
 if __name__ == "__main__":
     if isfile(NMAP):
