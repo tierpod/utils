cbb8f888025e41f75b9ca229b20d95f454c68f74
Pavel Podkorytov <pod.pavel@gmail.com>
2015-07-01 10:45:14 +0500

    Added bin/mac-normalize.py

---
+++ bin/bin/mac-normalize.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Normalize mac address to format: 'ff:ff:ff:ff:ff:ff'
+
+Example: echo 'MAC_ADDRESS' | mac-normalize.py
+'''
+
+import sys
+
+N = 2
+
+data = sys.stdin.readlines()
+for input_line in data:
+    line = input_line.lower().translate(None, ' :.\n')
+    print ':'.join([line[i:i+N] for i in range(0, len(line), N)])
