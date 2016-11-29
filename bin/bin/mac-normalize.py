480d663b5101a8e4123fdf1f89c689c2f902a4ea
Pavel Podkorytov <pod.pavel@gmail.com>
2016-11-29 17:30:29 +0500

    Update mac-normalize

--- bin/bin/mac-normalize.py
+++ bin/bin/mac-normalize.py
@@ -4,18 +4,36 @@
 '''
 Normalize mac address to format: 'ff:ff:ff:ff:ff:ff'
 
-Example: echo 'MAC_ADDRESS' | mac-normalize.py
+Examples:
+mac-normalize.py MAC_ADDRESS
+echo 'MAC_ADDRESS' | mac-normalize.py
 '''
 
 import sys
 
 N = 2
 
+def mac_normalize(mac):
+    raw_mac = mac.lower().translate(None, ' :.-\n')
+    # check length
+    if len(raw_mac) != 12:
+        print 'Error: wrong input length: {0} {1}/16'.format(raw_mac, len(raw_mac))
+        exit(1)
+    list_mac = [raw_mac[i:i+N] for i in range(0, len(raw_mac), N)]
+    # check elements in list
+    for elem in list_mac:
+        try:
+            int(elem, 16)
+        except ValueError:
+            print 'Error: wrong element in mac: {0} {1}'.format(elem, raw_mac)
+            exit(2)
+    return ':'.join([raw_mac[i:i+N] for i in range(0, len(raw_mac), N)])
+
+
 if len(sys.argv) == 2:
     data = sys.argv[1].split('\n')
+    for input_line in data:
+        print mac_normalize(input_line)
 else:
-    data = sys.stdin.readlines()
+    print __doc__.strip()
 
-for input_line in data:
-    line = input_line.lower().translate(None, ' :.-\n')
-    print ':'.join([line[i:i+N] for i in range(0, len(line), N)])
